172f186cb46b2ab52ef76af1cc453988ed193bc7
ParkSiHyeon <s.hyeon.dev@gmail.com>
2020-10-12 15:31:48 +0900

    마우스 이벤트

---
+++ bootstrapTest/WebContent/js/jqueryex04.js
@@ -0,0 +1,15 @@
+$(function() {
+	$("html, body").on("DOMMouseScroll", function(e){
+		console.log("e.originalEvent.detail:" +e.originalEvent.detail);
+	});
+	$("html, body").on("mousewheel", function(e){
+		console.log("e.originalEvent.wheelDelta:" +e.originalEvent.wheelDelta);
+	});
+	$("html, body").on("mousewheel DOMMouseScroll", function(e){
+		if(e.originalEvent.wheelDelta == 120 || e.originalEvent.detail == -3){
+			$(".container").text("up scroll event");
+		}else if(e.originalEvent.wheelDelta == -120 || e.originalEvent.detail ==3) {
+			$(".container").text("down scroll event");
+		}
+	});
+});
